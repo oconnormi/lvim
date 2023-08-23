c52cac4c54778b9a856555c8eaf24c11c4f15d9d
Michael O'Connor <oconnor.michael.stephen@gmail.com>
2023-08-22 23:39:36 -0400

    feat: Add neotest

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -44,6 +44,32 @@ lvim.plugins = {
       })
     end,
   },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/neotest-go",
+      -- Your other test adapters here
+    },
+    config = function()
+      -- get neotest namespace (api call creates or returns namespace)
+      local neotest_ns = vim.api.nvim_create_namespace("neotest")
+      vim.diagnostic.config({
+        virtual_text = {
+          format = function(diagnostic)
+            local message =
+              diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+            return message
+          end,
+        },
+      }, neotest_ns)
+      require("neotest").setup({
+        -- your neotest config here
+        adapters = {
+          require("neotest-go"),
+        },
+      })
+    end,
+  },
 
   {
     "folke/trouble.nvim",
